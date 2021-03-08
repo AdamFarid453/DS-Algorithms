75344f485da9d18c305f042d535dc7d17d95fd5a
Adam Farid <afarid@stevens.edu>
2021-03-08 13:03:03 -0500

    master

---
+++ BST/257.py
@@ -0,0 +1,15 @@
+"""
+    Author: Adam Farid
+    Problem: 257 Binary Tree Paths
+    Given a binary tree, return all root-to-leaf paths.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+        print(root)
